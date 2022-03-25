f8059507106ef1dae76428e4b23be9c96b95369e
srji <iosergio.valbuena@gmail.com>
2022-03-24 22:31:09 -0700

    dropdown added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
+//fetch function, promise, try/catch
 const fetchData = (url_api) => {
     return new Promise((resolve, reject) => {
         const xhttp = new XMLHttpRequest();
@@ -16,32 +15,27 @@ const fetchData = (url_api) => {
     });
 }
 
+//url from the API 
 const API = 'https://deepblue.camosun.bc.ca/~c0180354/ics128/lab09/lab09_superheroes';
 
-document.getElementById("demo").innerHTML = `
-<tr>
-<th>Superhero</th>
-<th>Publisher</th>
-<th>Alter Ego</th>
-<th>First Appearance</th>
-<th>Characters</th>
-</tr>
-`;
 
 function loadDoc(){
+    //clear tje table and create <th>table headers</th>
+    document.getElementById("demo").innerHTML = `
+        <tr>
+        <th>Superhero</th>
+        <th>Publisher</th>
+        <th>Alter Ego</th>
+        <th>First Appearance</th>
+        <th>Characters</th>
+        </tr>
+        `;
+
+    //functionToCall API and print 
     const anotherFunction = async (url_api) => {
         try {
             const data = await fetchData(url_api)
-            //console.log(data); //prints all 
-            //console.log(data[1]); //print reference number on the []
-
-            //console.log(data); //print reference number on the []
-            
             console.log(data); //print reference number on the []
-            //console.log('ONE');
-            //let name1 = data.superhero
-            //console.log(name1); 
-            
             for (let i = 0; i < data.length; i++) {
                 const superhero = data[i].superhero;
                 const publisher = data[i].publisher;
@@ -66,12 +60,8 @@ function loadDoc(){
                     console.log('aefaefaf');
                 }else{
                     console.log('no publisher found');
-                }
-
-                
-                
+                }                
             }
-
 
         } catch(error){
             console.error(error)
@@ -85,21 +75,18 @@ function loadDoc(){
     //console.log('After');
 
 }
-
-var e = document.getElementById("supers");
-var strUser = e.options[e.selectedIndex].value;
-
-if(strUser === 'all') {
-    console.log('aqui lo tienes pa q lo goces '+strUser);
-} else if(strUser === 'marvel'){
-console.log('MARVEL!!! '+strUser);
-}
-
-
-
-
 function loadM(){
-    document.getElementById("demo").innerHTML = "" 
+    document.getElementById("demo").innerHTML = `
+    <tr>
+    <th>Superhero</th>
+    <th>Publisher</th>
+    <th>Alter Ego</th>
+    <th>First Appearance</th>
+    <th>Characters</th>
+    </tr>
+    `;
+
+
     const anotherFunction = async (url_api) => {
         try {
             const data = await fetchData(url_api)
@@ -129,20 +116,74 @@ function loadM(){
                 console.log('no publisher found');
             }
         }
-
-
-
-
         } catch(error){
             console.error(error)
         }
     }
-
-
-
-    //console.log('Before');
     anotherFunction(API);
-    //console.log('After');
 
 }
+function loadD(){
+    document.getElementById("demo").innerHTML = `
+    <tr>
+    <th>Superhero</th>
+    <th>Publisher</th>
+    <th>Alter Ego</th>
+    <th>First Appearance</th>
+    <th>Characters</th>
+    </tr>
+    `;
+
+
+    const anotherFunction = async (url_api) => {
+        try {
+            const data = await fetchData(url_api)
+
+                for (let i = 0; i < data.length; i++) {                
+                    const publisher = data[i].publisher;
+                if(publisher === 'DC Comics'){
+                    const superhero = data[i].superhero;
+                    const publisher = data[i].publisher;
+                    const alterEgo = data[i].alter_ego;
+                    const firstAppearance = data[i].first_appearance;
+                    const characters = data[i].characters;
+
+                    document.getElementById("demo").innerHTML += 
+                    `
+                    <tr>
+                        <td>${superhero} </td>
+                        <td>${publisher}</td>
+                        <td>${alterEgo}</td>
+                        <td>${firstAppearance}</td>
+                        <td>${characters}</td>
+                    </tr>
+                    `;  
+            }else if(publisher === 'DC Comics'){
+                console.log('aefaefaf');
+            }else{
+                console.log('no publisher found');
+            }
+        }
+        } catch(error){
+            console.error(error)
+        }
+    }
+    anotherFunction(API);
+
+}
+
+
+function selectPublisher(element){
+    const publisherOption = element.options[element.selectedIndex].value;
+    if (publisherOption === 'all') {
+        loadDoc()
+    } else if (publisherOption === 'marvel') {
+        loadM()
+    }else if (publisherOption === 'dc') {
+        loadD()
+    }
+}
+
+
+
 
